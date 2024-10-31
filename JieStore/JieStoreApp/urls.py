60bcc623cc08171eeecfb0af51303a9933bb759b
Jiecheng Yao <jiy921@lehigh.edu>
2024-10-31 18:34:36 -0400

    Fixed cat animation

--- JieStore/JieStoreApp/urls.py
+++ JieStore/JieStoreApp/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('items/', views.item_list, name='storefront'),
     path('items/cart/', views.cart_view, name='cart_view'),
+    path('items/add_to_cart/', views.add_to_cart, name='add_to_cart'),
     path('checkout/', views.checkout, name='checkout'), 
 ]
 
